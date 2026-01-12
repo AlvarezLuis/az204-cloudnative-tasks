f751752a7a8610353af8e21780f97f46834a3e84
Luis Alvarez <luisalvarez9103@gmail.com>
2026-01-12 16:04:55 -0500

    Agregar archivos de proyecto.

---
+++ CloudNativeTasks.Api/Controllers/TasksController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudNativeTasks.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TasksController : ControllerBase
+    {
+        private readonly List<string> _tasks = ["tarea 1", "tarea 2"];
+
+        [HttpGet]
+        public IEnumerable<string> GetTasks()
+        {
+            return _tasks;
+        }
+
+        [HttpPost]
+        public ActionResult CreateTask(string value)
+        {
+            _tasks.Add(value);
+            return Ok();
+        }
+    }
+}
